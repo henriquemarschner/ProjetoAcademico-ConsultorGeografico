5c18e97e5a3dba2bc6d676a101694d92361ddbd5
Henrique Marschner <106743107+henriquemarschner@users.noreply.github.com>
2026-08-18 19:22:52 -0300

    Add files via upload

---
+++ FrmConsultas.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ConsultaPaises
+{
+    public partial class FrmConsultas : ConsultaPaises.FrmBase
+    {
+        public FrmConsultas()
+        {
+            InitializeComponent();
+            // Conectar eventos manualmente
+            if (this.btnExcluir != null)
+                this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+        }
+
+        public virtual void Incluir()
+        {
+
+        }
+
+        public virtual void Alterar()
+        {
+
+        }
+
+        public virtual void Excluir()
+        {
+
+        }
+
+        public virtual void Pesquisar()
+        {
+
+        }
+
+        public virtual void BloqueiaTxt()
+        {
+
+        }
+
+        public virtual void DesbloqueiaTxt()
+        {
+
+        }
+
+        public virtual void setFrmCadastro(object obj)
+        {
+
+        }
+
+        protected void btnIncluir_Click(object sender, EventArgs e)
+        {
+            Incluir();
+        }
+
+        protected void btnAlterar_Click(object sender, EventArgs e)
+        {
+            Alterar();
+        }
+
+        protected void btnExcluir_Click(object sender, EventArgs e)
+        {
+            Excluir();
+        }
+
+        protected void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            Pesquisar();
+        }
+
+    }
+}
---
+++ FrmPrincipal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsultaPaises
+{
+    public partial class FrmPrincipal : Form
+    {
+        private Interface aInter;
+        private Paises oPais;
+        private Estados oEstado;
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            aInter = new Interface();
+            oPais = new Paises();
+            oEstado = new Estados();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void estadosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            aInter.PecaEstados(oEstado, null);
+        }
+
+        private void cidadesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            aInter.PecaCidades(null, null);
+        }
+
+        private void paisesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            aInter.PecaPaises(oPais, null);
+        }
+    }
+}
