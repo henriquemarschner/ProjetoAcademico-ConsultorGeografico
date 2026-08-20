d3f1179eb9c7c223f8951c5b91a621eaa25b81cc
henriquemarschner <henrique.marchner@gmail.com>
2026-08-20 10:22:04 -0300

    Adição Classe Pessoa

---
+++ Pessoa.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaPaises
+{
+    public class Pessoa
+    {
+        protected int idade;
+        protected string nome;
+        protected char sexo;
+
+        public Pessoa()
+        {
+            idade = 0;
+            nome = "";
+            sexo = ' ';
+        }
+
+        public Pessoa(int idade, string nome, char sexo)
+        {
+            this.idade = idade;
+            this.nome = nome;
+            this.sexo = sexo;
+        }
+
+        public int Idade
+        {
+            get => idade;
+            set => idade = value;
+        }
+
+        public string Nome
+        {
+            get => nome;
+            set => nome = value;
+        }
+
+        public char Sexo
+        {
+            get => sexo;
+            set => sexo = value;
+        }
+
+        public Pessoa Clone()
+        {
+            return new Pessoa(idade, nome, sexo);
+        }
+    }
+}
