67376e05245ffffdb4cc5b5660fefa6428b19915
henriquemarschner <henrique.marchner@gmail.com>
2026-08-20 10:51:01 -0300

    Adição classe Controller

---
+++ Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaPaises
+{
+    public class Controller
+    {
+        public virtual string CarregarObj(object obj)
+        {
+            return null;
+        }
+
+        public virtual List<object> CarregarCol(object obj)
+        {
+            return null;
+        }
+
+        public virtual string Salvar(object obj)
+        {
+            return null;
+        }
+
+        public virtual string Excluir(object obj)
+        {
+            return null;
+        }
+
+        public virtual List<object> Pesquisar(object obj)
+        {
+            return null;
+        }
+
+    }
+}
