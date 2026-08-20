d230553d40625e32663f0dadbeafea45faf58030
Henrique Marschner <106743107+henriquemarschner@users.noreply.github.com>
2026-08-20 10:08:47 -0300

    Delete Reação/site-museu-master directory

--- Reação/site-museu-master/src/app/Podcast.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function Podcast() {
-  return (
-    <div className="font-sans bg-white text-gray-900 min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Página de Podcast</h1>
-    </div>
-  );
-}
