c02a817ca65fff2422daeebe3cbf810b227a883b
henriquemarschner <henrique.marchner@gmail.com>
2026-08-25 19:28:13 -0300

    Adição Classe e Controller Cargo

---
+++ Classes/Cargo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaPaises.Classes
+{
+    public class Cargo
+    {
+        protected char exigeCNH;
+        protected char cargo;
+
+        public Cargo()
+        {
+            exigeCNH = ' ';
+            cargo = ' ';
+        }
+        public Cargo(char exigeCNH, char cargo)
+        {
+            this.exigeCNH = exigeCNH;
+            this.cargo = cargo;
+        }
+
+        public char ExigeCNH
+        {
+            get => exigeCNH;
+            set => exigeCNH = value;
+        }
+
+        public char Cargos
+        {
+            get => cargo;
+            set => cargo = value;
+        }
+    }
+}
---
+++ Controller/ControllerCargo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaPaises.Controller
+{
+    public class ControllerCargo
+    {
+            public virtual string CarregarObj(object obj)
+            {
+                return null;
+            }
+
+            public virtual List<Object> CarregarCol(object obj)
+            {
+                return null;
+            }
+
+        public virtual string Salvar(object obj)
+            {
+                return null;
+            }
+
+            public virtual string Excluir(object obj)
+            {
+                return null;
+            }
+
+            public virtual List<Object> Pesquisar(object obj)
+            {
+                return null;
+            }
+    }
+}
