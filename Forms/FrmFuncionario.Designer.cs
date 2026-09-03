5c061d5a7ddb0758ea057aa2db4fdf2dfe09dfe1
henriquemarschner <henrique.marchner@gmail.com>
2026-09-03 20:38:28 -0300

    Implementação do BD e SQLQuery adicionando estados e paises

---
+++ Forms/FrmFuncionario.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace ConsultaPaises.Controller
+{
+    partial class FrmFuncionario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblCargo = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblDados = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.lblCEP = new System.Windows.Forms.Label();
+            this.lblComp = new System.Windows.Forms.Label();
+            this.lblCid = new System.Windows.Forms.Label();
+            this.lblBairro = new System.Windows.Forms.Label();
+            this.lblNum = new System.Windows.Forms.Label();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 259);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(81, 316);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(213, 157);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 0;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(213, 205);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 0;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(213, 259);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 0;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(81, 157);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 208);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "label1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(242, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "label1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(242, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "label1";
+            // 
+            // lblCargo
+            // 
+            this.lblCargo.AutoSize = true;
+            this.lblCargo.Location = new System.Drawing.Point(110, 297);
+            this.lblCargo.Name = "lblCargo";
+            this.lblCargo.Size = new System.Drawing.Size(44, 16);
+            this.lblCargo.TabIndex = 1;
+            this.lblCargo.Text = "Cargo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(242, 240);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "label1";
+            // 
+            // lblDados
+            // 
+            this.lblDados.AutoSize = true;
+            this.lblDados.Location = new System.Drawing.Point(151, 95);
+            this.lblDados.Name = "lblDados";
+            this.lblDados.Size = new System.Drawing.Size(102, 20);
+            this.lblDados.TabIndex = 1;
+            this.lblDados.Text = "Dados Pessoais";
+            this.lblDados.UseCompatibleTextRendering = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(567, 95);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 20);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Endereço";
+            this.label8.UseCompatibleTextRendering = true;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(478, 256);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 22);
+            this.textBox8.TabIndex = 0;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(478, 205);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 22);
+            this.textBox9.TabIndex = 0;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(610, 205);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 22);
+            this.textBox10.TabIndex = 0;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(478, 157);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 22);
+            this.textBox11.TabIndex = 0;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(610, 253);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 22);
+            this.textBox12.TabIndex = 0;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(542, 317);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(100, 22);
+            this.textBox14.TabIndex = 0;
+            // 
+            // lblCEP
+            // 
+            this.lblCEP.AutoSize = true;
+            this.lblCEP.Location = new System.Drawing.Point(507, 186);
+            this.lblCEP.Name = "lblCEP";
+            this.lblCEP.Size = new System.Drawing.Size(34, 16);
+            this.lblCEP.TabIndex = 1;
+            this.lblCEP.Text = "CEP";
+            // 
+            // lblComp
+            // 
+            this.lblComp.AutoSize = true;
+            this.lblComp.Location = new System.Drawing.Point(486, 237);
+            this.lblComp.Name = "lblComp";
+            this.lblComp.Size = new System.Drawing.Size(91, 16);
+            this.lblComp.TabIndex = 1;
+            this.lblComp.Text = "Complemento";
+            // 
+            // lblCid
+            // 
+            this.lblCid.AutoSize = true;
+            this.lblCid.Location = new System.Drawing.Point(507, 138);
+            this.lblCid.Name = "lblCid";
+            this.lblCid.Size = new System.Drawing.Size(51, 16);
+            this.lblCid.TabIndex = 1;
+            this.lblCid.Text = "Cidade";
+            // 
+            // lblBairro
+            // 
+            this.lblBairro.AutoSize = true;
+            this.lblBairro.Location = new System.Drawing.Point(639, 186);
+            this.lblBairro.Name = "lblBairro";
+            this.lblBairro.Size = new System.Drawing.Size(43, 16);
+            this.lblBairro.TabIndex = 1;
+            this.lblBairro.Text = "Bairro";
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(639, 234);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(55, 16);
+            this.lblNum.TabIndex = 1;
+            this.lblNum.Text = "Numero";
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.AutoSize = true;
+            this.lblEnd.Location = new System.Drawing.Point(571, 298);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(66, 16);
+            this.lblEnd.TabIndex = 1;
+            this.lblEnd.Text = "Endereco";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(213, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(610, 156);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // FrmFuncionario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblEnd);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblCargo);
+            this.Controls.Add(this.lblNum);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblBairro);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblCid);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblComp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblDados);
+            this.Controls.Add(this.lblCEP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox1);
+            this.Name = "FrmFuncionario";
+            this.Text = "FrmFuncionario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        protected System.Windows.Forms.TextBox textBox2;
+        protected System.Windows.Forms.TextBox textBox3;
+        protected System.Windows.Forms.TextBox textBox4;
+        protected System.Windows.Forms.TextBox textBox5;
+        protected System.Windows.Forms.TextBox textBox6;
+        protected System.Windows.Forms.TextBox textBox7;
+        protected System.Windows.Forms.TextBox textBox1;
+        protected System.Windows.Forms.Label label1;
+        protected System.Windows.Forms.Label label2;
+        protected System.Windows.Forms.Label label3;
+        protected System.Windows.Forms.Label label4;
+        protected System.Windows.Forms.Label label5;
+        protected System.Windows.Forms.Label lblCargo;
+        protected System.Windows.Forms.Label label7;
+        protected System.Windows.Forms.Label lblDados;
+        protected System.Windows.Forms.Label label8;
+        protected System.Windows.Forms.TextBox textBox8;
+        protected System.Windows.Forms.TextBox textBox9;
+        protected System.Windows.Forms.TextBox textBox10;
+        protected System.Windows.Forms.TextBox textBox11;
+        protected System.Windows.Forms.TextBox textBox12;
+        protected System.Windows.Forms.TextBox textBox14;
+        protected System.Windows.Forms.Label lblCEP;
+        protected System.Windows.Forms.Label lblComp;
+        protected System.Windows.Forms.Label lblCid;
+        protected System.Windows.Forms.Label lblBairro;
+        protected System.Windows.Forms.Label lblNum;
+        protected System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ QueryInicial.sql
@@ -0,0 +1,14 @@
+﻿INSERT INTO Paises (pais, sigla, ddi, moeda) VALUES
+('BRASIL', 'BR', '55', 'R$');
+
+SELECT * FROM Paises;
+
+select * from Estados;
+
+select * from Paises;
+
+insert into Estados(estado, uf, idpais)
+	values('PARANA','PR',10);
+insert into Estados(estado, uf, idpais)
+	values('SAO PAULO','SP',10);
+commit;
